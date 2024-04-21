cc8dff6094f648851b448d949fe7e66523f2ceb3
10kumaranurag01 <kumarkas1515@gmail.com>
2024-04-21 12:16:33 +0530

    cl in merchant

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -3,6 +3,7 @@
 import { useBalance } from "@repo/store/useBalance";
 
 export default function() {
+  console.log("hi there");
   const balance = useBalance();
   return <div>
     hi there {balance}
